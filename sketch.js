a2e06d1952fbe382c1338db9520fc022397c4239
camille-granier <granierc@protonmail.com>
2023-02-01 13:36:49 +1100

    initialising tutorial

---
+++ sketch.js
@@ -0,0 +1,27 @@
+// Global variables
+let circleX = 300;
+let circleY = 0;
+let xSpeed = 1;
+let ySpeed = 1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  // Bouncing ball
+  ellipse(circleX, circleY, 120);
+  
+  // TODO: Increment the x position of the ball
+
+circleX += xSpeed
+  // TODO: Increment the y position of the ball
+
+circleY += ySpeed
+  // TODO: If statment to inverse direction when ball hits left or right edge
+
+
+  // TODO: If statment to inverse direction when ball hits top or bottom edge
+
+}
