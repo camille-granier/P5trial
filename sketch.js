a5694a00d1045b513a9b7892d6804d7992ac5ebf
camille-granier <granierc@protonmail.com>
2023-02-05 19:04:59 +1100

    tutorial transformations rectangle

--- sketch.js
+++ sketch.js
@@ -1,33 +1,22 @@
-let t;
-let increment = 0.001; 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  strokeWeight(2);
-  stroke(255);
-  noFill();
-  t = 0;
+  // Sets origin mode to be the rectangle's center
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(0, 15);
-makeCircle(4, 50, 600)
-  // TODO: Call the makeCircle() function with 4 for xPos, 50 for yPos and 600 for circleSize
+  background(0);
 
+  // Moves rectangle to center of canvas
+  translate(width / 2, height / 2);
 
-  // TODO: Call the makeCircle() function with 0 for xPos, 200 for yPos and 200 for circleSize
-
-makeCircle(0, 200, 200)
-  // TODO: Call the makeCircle() function with 100 for xPos, 200 for yPos and 1 for circleSize
-makeCircle(100, 200, 1)
-}
-
-function makeCircle(xPos, yPos, circleSize) { 
-  let x = width * noise(t + xPos);
-  let y = height * noise(t + yPos);
-  let size = width * noise(t + circleSize);
-
-  ellipse(x, y, size, size);
-
-  t += increment;
+  // Creates multiple rotating rectangles
+  for (let x = 500; x > 0; x -= 10) {
+      // Makes height the same as width so it's a square
+      let y = x;
+      strokeWeight(0.5);
+      rotate(radians(frameCount / 50));
+      fill(255, x, 255);
+      rect(0, 0, x, y);
+  }
 }
