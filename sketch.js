df720225b0bba44aa0f6e1e1ece048a5c0fbe850
camille-granier <granierc@protonmail.com>
2023-02-08 11:09:16 +1100

    tutorial mousepressed event

--- sketch.js
+++ sketch.js
@@ -1,17 +1,37 @@
+let fillValue; // Used to modify the ellipse's fill color
+let shapeScale; // Used to scale the size of the ellipse
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
-  // TODO: Move background() in draw() function here
-  background(75);
-}
+  fillValue = 0;
+  shapeScale = 50;
+} 
 
 function draw() {
-  // TODO: Move below background() to setup()
+  background(75, 50);
 
+  // TODO: Add if statement to check if mouseIsPressed and set fill colors accordingly
+if (mouseIsPressed) {
+  fill(fillValue, 0, 0, 50)
+} else {
+  fill(0, 0, fillValue, 50);
+}
 
-  // TODO: Set fill() using mouseX and mouseY variables
-fill(mouseX, 135, mouseY)
-  // TODO: Draw an ellipse using mouseX and mouseY for the x and y position
-ellipse(mouseX, mouseY, 75, 75)
+  ellipse(width / 2, height / 2, shapeScale, shapeScale);
+}
+
+// TODO: Create a mouseMoved() function and inside the function, set fillValue to a random number between 0 and 255
+function mouseMoved() {
+  fillValue = random(0, 255)
+}
+
+// TODO: Create a mousePressed() function and inside the function, modify shapeScale variable
+function mousePressed() {
+  if (shapeScale < width) {
+  shapeScale  *= 2
+  } else {
+    shapeScale = 50
+  }
 }
